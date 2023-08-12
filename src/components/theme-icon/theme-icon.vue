4f03b81c8d87bf8516321598ff42a83f56005d3c
cqq <945327638@qq.com>
2023-08-12 10:52:51 +0800

    perf: 优化themeIcon主题颜色获取逻辑

--- src/components/theme-icon/theme-icon.vue
+++ src/components/theme-icon/theme-icon.vue
@@ -3,14 +3,20 @@
 </template>
 <script setup lang="ts">
 import { useTheme } from '@/hooks/useTheme'
-const { getThemeColor } = useTheme()
+const { tmPiniaStore, getThemeColor } = useTheme()
 const props = defineProps({
   color: {
     type: String,
     default: ''
   }
 })
-const themeColor = getThemeColor()
+const themeColor = computed(() => {
+  let themeColorName = tmPiniaStore.tmStore.color
+  if (themeColorName) {
+    return getThemeColor()
+  }
+  return getThemeColor()
+})
 </script>
 
 <style scoped></style>
