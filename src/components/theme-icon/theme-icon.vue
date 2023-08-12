0627f180d3737770f41a653f4b06f4b16eb6845a
cqq <945327638@qq.com>
2023-08-12 10:30:29 +0800

    feat: 增加主题色icon组件

---
+++ src/components/theme-icon/theme-icon.vue
@@ -0,0 +1,16 @@
+<template>
+  <uni-icons v-bind="$attrs" :color="color || themeColor"></uni-icons>
+</template>
+<script setup lang="ts">
+import { useTheme } from '@/hooks/useTheme'
+const { getThemeColor } = useTheme()
+const props = defineProps({
+  color: {
+    type: String,
+    default: ''
+  }
+})
+const themeColor = getThemeColor()
+</script>
+
+<style scoped></style>
