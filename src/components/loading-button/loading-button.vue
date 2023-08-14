5848879cabcce9f8218c199da10eb5268b666411
cqq <945327638@qq.com>
2023-08-14 17:40:11 +0800

    feat: 封装时间loading-button组件

---
+++ src/components/loading-button/loading-button.vue
@@ -0,0 +1,28 @@
+<template>
+  <tm-button @click="clickHandle" v-bind="$attrs" :loading="loading"></tm-button>
+</template>
+<script setup lang="ts">
+import { defineProps } from 'vue'
+const props = defineProps({
+  clickFun: {
+    type: Function,
+    default: () => {}
+  }
+})
+const loading = ref(false)
+async function clickHandle() {
+  try {
+    loading.value = true
+    await props.clickFun()
+    // loading.value = false
+    setTimeout(() => {
+      loading.value = false
+    }, 100)
+  } catch (error) {
+    console.log(error)
+    loading.value = false
+  }
+}
+</script>
+
+<style scoped></style>
