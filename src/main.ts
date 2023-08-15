609d6c81078e18fdc1705db17234265f84a0255e
cqq <945327638@qq.com>
2023-08-15 16:40:49 +0800

    feat: 增加全局样式表

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import pinia from './store/index'
 import tmui from './tmui'
 import App from './App.vue'
 import { tmConfig } from './config/tmConfig'
+import './style/global.scss'
 export function createApp() {
   const app = createSSRApp(App)
   app.use(pinia)
