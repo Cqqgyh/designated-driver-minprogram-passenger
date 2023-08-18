007b9723abb196623e0a8e278e44c2a78ac2159e
cqq <945327638@qq.com>
2023-08-18 15:05:03 +0800

    feat: loading-button支持type属性传入

--- src/components/loading-button/loading-button.vue
+++ src/components/loading-button/loading-button.vue
@@ -1,15 +1,45 @@
 <template>
-  <tm-button @click="clickHandle" v-bind="$attrs" :loading="loading"></tm-button>
+  <tm-button @click="clickHandle" v-bind="$attrs" :loading="loading" :color="color"></tm-button>
 </template>
 <script setup lang="ts">
-import { defineProps } from 'vue'
+import { PropType } from 'vue'
+type buttonType = 'primary' | 'success' | 'warning' | 'danger' | 'info'
 const props = defineProps({
   clickFun: {
     type: Function,
     default: () => {}
+  },
+  type: {
+    type: String as PropType<buttonType>,
+    default: 'primary'
   }
 })
 const loading = ref(false)
+const color = computed(() => {
+  const typeColorList = [
+    {
+      type: 'primary',
+      color: ''
+    },
+    {
+      type: 'success',
+      color: 'green'
+    },
+    {
+      type: 'warning',
+      color: 'deep-orange'
+    },
+    {
+      type: 'danger',
+      color: 'red'
+    },
+    {
+      type: 'info',
+      color: 'grey-darken-1'
+    }
+  ]
+  return typeColorList.find((item) => item.type === props.type)?.color || ''
+})
 async function clickHandle() {
   try {
     loading.value = true
