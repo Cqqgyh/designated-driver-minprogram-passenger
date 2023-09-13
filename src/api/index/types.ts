a700976c1223ce67d79ba5b19efaa4b7fa4000cc
cqq <945327638@qq.com>
2023-09-13 17:08:34 +0800

    feat: 增加order接口定义类型

--- src/api/index/types.ts
+++ src/api/index/types.ts
@@ -1,37 +1,37 @@
 // 地图属性 https://uniapp.dcloud.net.cn/component/map.html#
 export interface IMapProps {
-    longitude: number
-    latitude: number
-    scale: number
-    theme: string
-    minScale: number
-    maxScale: number
-    layerStyle: number | string
-    markers: Array<any>
-    polyline: Array<any>
-    circles: Array<any>
-    controls: Array<any>
-    includePoints: Array<any>
-    enable3D: boolean
-    showCompass: boolean
-    enableZoom: boolean
-    enableScroll: boolean
-    enableRotate: boolean
-    enableOverlooking: boolean
-    enableSatellite: boolean
-    enableTraffic: boolean
-    enablePoi: boolean
-    enableBuilding: boolean
-    showLocation: boolean
-    polygons: Array<any>
-    enableIndoorMap: boolean
-    markertap: Function
-    labeltap: Function
-    callouttap: Function
-    controltap: Function
-    regionchange: Function
-    tap: Function
-    updated: Function
-    anchorpointtap: Function
-    poitap: Function
+  longitude: number
+  latitude: number
+  scale: number
+  theme: string
+  minScale: number
+  maxScale: number
+  layerStyle: number | string
+  markers: Array<any>
+  polyline: Array<any>
+  circles: Array<any>
+  controls: Array<any>
+  includePoints: Array<any>
+  enable3D: boolean
+  showCompass: boolean
+  enableZoom: boolean
+  enableScroll: boolean
+  enableRotate: boolean
+  enableOverlooking: boolean
+  enableSatellite: boolean
+  enableTraffic: boolean
+  enablePoi: boolean
+  enableBuilding: boolean
+  showLocation: boolean
+  polygons: Array<any>
+  enableIndoorMap: boolean
+  markertap: () => void
+  labeltap: () => void
+  callouttap: () => void
+  controltap: () => void
+  regionchange: () => void
+  tap: () => void
+  updated: () => void
+  anchorpointtap: () => void
+  poitap: () => void
 }
