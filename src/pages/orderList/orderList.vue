ea077b72ec6075165fb3b414bbfb4cc0b52e3b69
cqq <945327638@qq.com>
2023-09-22 08:51:05 +0800

    feat: 增加静态订单列表

--- src/pages/orderList/orderList.vue
+++ src/pages/orderList/orderList.vue
@@ -9,16 +9,56 @@
       activeColor="white"
       :list="navList"
       v-model="currentIndex"
-      :defaultValue="0"
+      :defaultValue="navList[0]?.id || 0"
+      @change="navChangeHandle"
     ></tm-segtab>
     <tm-scrolly _style="height: 90vh" v-model="refreshStatus" v-model:bottom-value="refreshBottomStatus" @refresh="refresh" @bottom="refreshBottom">
       <view v-for="(item, index) in [...navList, ...navList, ...navList, ...navList, ...navList]" :key="index">
-        <tm-coupon color="pink" linear="right" linear-deep="accent" mainColor="yellow" font-color="" extra>
-          <template v-slot:extra>
-            <tm-text :font-size="22" _class="opacity-7" label="1.使用不受限制"></tm-text>
-            <tm-text :font-size="22" _class="opacity-7" label="2.使用不受限制"></tm-text>
-          </template>
-        </tm-coupon>
+        <tm-sheet :margin="[25]" :round="3" :shadow="2" _class="text-size-m">
+          <tm-cell :margin="[0, 0]">
+            <template #title>
+              <tm-text _class="text-weight-b" label="代驾订单"></tm-text>
+            </template>
+            <template #right>
+              <text class="ml-10 text-gray-1">{{ '已完成' }}</text>
+            </template>
+          </tm-cell>
+          <view>
+            <tm-cell :margin="[0, 0]">
+              <template #title>
+                <view class="flex flex-row flex-row-center-start text-size-m">
+                  <view style="height: 20rpx; width: 20rpx; background-color: #93da5f; border-radius: 50%"></view>
+                  <text class="ml-20 text-overflow-1">
+                    {{ '北京天安门北京天安门北京天安门北京天安门北京天安门北京天安门北京天安门北京天安门北京天安门北京天安门' }}
+                  </text>
+                </view>
+              </template>
+              <template #right>
+                <text class="ml-10 text-gray-1">{{ '' }}</text>
+              </template>
+            </tm-cell>
+            <tm-cell :margin="[0, 0]">
+              <template #title>
+                <view class="flex flex-row flex-row-center-start">
+                  <view style="height: 20rpx; width: 20rpx; background-color: #48b6fc; border-radius: 50%"></view>
+                  <text class="ml-20 text-overflow-1">
+                    {{ '北京天安门北京天安门北京天安门北京天安门北京天安门北京天安门北京天安门北京天安门北京天安门北京天安门' }}
+                  </text>
+                </view>
+              </template>
+              <template #right>
+                <text class="ml-10 text-gray-1">{{ '' }}</text>
+              </template>
+            </tm-cell>
+          </view>
+          <!--      订单创建时间-->
+          <tm-cell :margin="[0, 0]">
+            <template #title>
+              <tm-text color="grey" label="2023/08/15 16:04:00"></tm-text>
+            </template>
+            <template #right></template>
+          </tm-cell>
+        </tm-sheet>
       </view>
     </tm-scrolly>
   </tm-app>
@@ -31,22 +71,27 @@ const { themeColor } = useTheme()
 const currentIndex = ref(0)
 const navList = ref([
   {
-    id: 0,
+    id: 111,
     text: '全部'
   },
   {
     id: 1,
-    text: '未使用'
+    text: '行程中'
   },
   {
     id: 2,
-    text: '已使用'
+    text: '已完成'
   },
   {
     id: 3,
-    text: '已过期'
+    text: '已取消'
   }
 ])
+// 导航栏切换
+function navChangeHandle(index: number) {
+  console.log(index)
+  console.log('currentIndex', currentIndex.value)
+}
 //#region <下拉刷新，上拉加载>
 const refreshStatus = ref(false)
 const refreshBottomStatus = ref(false)
