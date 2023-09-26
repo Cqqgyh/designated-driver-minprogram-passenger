3ff5fb353739f721ebaa116113053801c0bece75
cqq <945327638@qq.com>
2023-09-26 17:23:29 +0800

    perf: 增加设置开始时间

--- src/hooks/useTimeIncrease.ts
+++ src/hooks/useTimeIncrease.ts
@@ -1,9 +1,11 @@
 import { ref } from 'vue'
 interface ITimeIncrease {
-  // 最大持续时长
+  // 最大持续时长 秒
   duration?: number
   // 回调函数,到达持续时长后执行
   callback?: () => void
+  //  从多少秒开始
+  startSeconds?: number
 }
 
 /**
@@ -45,6 +47,12 @@ export const useTimeIncrease = (param?: ITimeIncrease) => {
         stop()
         param.callback && param.callback()
       }
+      // 如果设置了从多少秒开始，设置初始开始时间
+      if (param && param.startSeconds) {
+        seconds.value = param.startSeconds % 60
+        minutes.value = Math.floor(param.startSeconds / 60)
+        hours.value = Math.floor(param.startSeconds / 3600)
+      }
     }, 1000)
   }
   const stop = () => {
@@ -57,8 +65,16 @@ export const useTimeIncrease = (param?: ITimeIncrease) => {
   }
   // 终止定时器，且重置时间
   const stopAndReset = () => {
-    clearInterval(timer.value)
+    clearInterval(timer.value || 0)
     reset()
+  }
+  // 设置开始时间
+  const setStartTime = (startSeconds: number) => {
+    if (startSeconds) {
+      seconds.value = startSeconds % 60
+      minutes.value = Math.floor(startSeconds / 60)
+      hours.value = Math.floor(startSeconds / 3600)
+    }
   }
 
   onUnmounted(() => {
@@ -72,6 +88,7 @@ export const useTimeIncrease = (param?: ITimeIncrease) => {
     start,
     stop,
     reset,
+    setStartTime,
     stopAndReset
   }
 }
