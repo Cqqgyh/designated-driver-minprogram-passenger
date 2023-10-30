a05da35af8e34e9521b5ec1032ece569a1812984
cqq <945327638@qq.com>
2023-10-30 17:18:03 +0800

    feat: 订单详情结合优惠券

--- src/pages/orderDetail/orderDetail.vue
+++ src/pages/orderDetail/orderDetail.vue
@@ -53,6 +53,57 @@
         <view class="flex-1 flex-col-center-center border-l-2 border-r-2 border-t-2 border-b-2">{{ item.value }}元</view>
       </view>
     </tm-sheet>
+    <!--    <view class="flex flex-row flex-row-bottom-end pay-container mb-10">-->
+    <!--   -->
+    <!--    </view>-->
+    <tm-sheet v-if="orderDetail.status === OrderStatus.UNPAID && !orderDetail.orderBillVo?.couponAmount" :padding="[0, 5, 0, 0]">
+      <tm-drawer>
+        <template>
+          <view v-for="item in couponList" :key="item.id">
+            <tm-coupon
+              :priceDetail="{
+                price: item.couponType === 1 ? item.amount : item.discount.toFixed(0), //价格金额
+                suffix: item.couponType === 1 ? '元' : '折', //后缀文本
+                prefix: '', //前缀文本
+                subtext: '' //小文本
+              }"
+              :rightDetail="{
+                title: item.couponType === 1 ? item.amount + '元' : item.discount.toFixed(0) + '折', //标题
+                subtitle: item.name, //副标题
+                time: item.expireTime //有效期时间文本
+              }"
+              color="pink"
+              linear="right"
+              linear-deep="accent"
+              mainColor="yellow"
+              font-color=""
+              extra
+            >
+              <template v-slot:thumb>
+                <text></text>
+              </template>
+              <template v-slot:btn>
+                <tm-checkbox @click="checkedCoupon(item)" color="pink" :round="24" v-model="item.isCheck"></tm-checkbox>
+              </template>
+              <template v-slot:extra>
+                <tm-text :font-size="22" _class="opacity-7" :label="item.description"></tm-text>
+              </template>
+            </tm-coupon>
+          </view>
+        </template>
+        <template v-slot:trigger>
+          <tm-cell
+            :round="3"
+            @click="getBestCouponHandle"
+            :margin="[0, 0, 0, 16]"
+            :rightText="currentCoupon?.name || '待选择'"
+            :titleFontSize="30"
+            title="选择优惠券"
+          ></tm-cell>
+        </template>
+      </tm-drawer>
+    </tm-sheet>
+
     <!--    支付 fixed b-0 -->
     <view class="flex flex-row flex-row-bottom-end pay-container mb-10">
       <loading-button color="red" :click-fun="handleReturn" :margin="[10]" :fontSize="35" :shadow="0" size="middle" label="返回"></loading-button>
@@ -70,8 +121,8 @@
   </tm-app>
 </template>
 <script setup lang="ts">
-import { IOrderDetail } from '@/api/order/types'
-import { getOrderDetail } from '@/api/order'
+import { ICoupon, IOrderDetail } from '@/api/order/types'
+import { getBestCoupon, getOrderDetail } from '@/api/order'
 import { usePayStore } from '@/store/modules/pay'
 import { OrderStatus } from '@/config/constEnums'
 const payStore = usePayStore()
@@ -110,10 +161,30 @@ const handlePay = () => {
   payStore.submitOrder({
     orderId: orderDetail.value.orderId,
     orderNo: orderDetail.value.orderNo,
-    customerCouponId: 0
+    customerCouponId: currentCoupon.value?.customerCouponId || 0
   })
 }
-
+//#region <优惠券相关>
+const couponList = ref<(ICoupon & { isCheck: boolean })[]>([])
+// 当前选中的优惠券Id
+const currentCoupon = ref({} as ICoupon & { isCheck: boolean })
+// 请求可用优惠券列表
+async function getBestCouponHandle() {
+  if (couponList.value.length) return
+  const res = await getBestCoupon(orderDetail.value.orderId)
+  couponList.value = res.data.map((item) => ({ ...item, isCheck: false }))
+}
+// 选中某个优惠券
+function checkedCoupon(item: ICoupon & { isCheck: boolean }) {
+  currentCoupon.value = item.isCheck ? ({} as unknown as ICoupon & { isCheck: boolean }) : item
+  // couponList中其他选项的isCheck设置为false
+  couponList.value.forEach((item2) => {
+    if (item2.customerCouponId !== item.customerCouponId) {
+      item2.isCheck = false
+    }
+  })
+}
+//#endregion
 onLoad(() => {
   console.log('props.orderId', props?.orderId)
   props?.orderId && getOrderDetailHandle(props?.orderId as unknown as string)
